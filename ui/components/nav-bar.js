618cee3653872a0d6799ff1cabcfd67214ee7c06
Chris Shepherd <thesheps@gmail.com>
2022-10-31 19:44:46 +0000

    feat(ui): adds navbar webcomponent

---
+++ ui/components/nav-bar.js
@@ -0,0 +1,33 @@
+import { css, html, LitElement } from "https://unpkg.com/lit?module";
+import styles from "../../styles/styles.js";
+
+class NavBar extends LitElement {
+  constructor() {
+    super();
+  }
+
+  static styles = [
+    ...styles,
+    css`
+      nav {
+        box-shadow: 0 0.2px 0;
+        background: #12191f;
+      }
+      a {
+        color: white;
+      }
+    `,
+  ];
+
+  render() {
+    const brand = html`<ul>
+      <li>
+        <h1><a href="/">greedy-http</a></h1>
+      </li>
+    </ul>`;
+
+    return html`<nav id="nav-bar" class="container-fluid">${brand}</nav>`;
+  }
+}
+
+customElements.define("nav-bar", NavBar);
