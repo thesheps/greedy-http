dd30af5dff9962adc9f9dd2c44ff248797880e22
Chris Shepherd <thesheps@gmail.com>
2022-11-03 09:08:39 +0000

    feat(tool): adds command line arg handling

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,2 @@
 #! /usr/bin/env node
-require("../api").run();
-require("../ui").run();
+require("../index")
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-require("./api").run();
-require("./ui").run();
+const args = require("args");
+
+args
+  .option("apiPort", "The API port", process.env.GREEDY_HTTP_API_PORT ?? 3000)
+  .option("uiPort", "The admin UI port", process.env.GREEDY_HTTP_UI_PORT ?? 3001)
+  .option("wsPort", "The local websocket port", process.env.GREEDY_HTTP_WS_PORT ?? 3002)
+  .option("ui", "Enables the admin interface", true)
+  .option("api", "Enables the api", true);
+
+const flags = args.parse(process.argv);
+flags.A && require("./api").run(flags.a, flags.w);
+flags.U && require("./ui").run(flags.u);
