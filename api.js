a4a97305e2d7a56b58ee2b141348c4c81e2775f9
Chris Shepherd <thesheps@gmail.com>
2022-11-03 09:07:51 +0000

    feat(): parameterises api and ui run functions

--- api.js
+++ api.js
@@ -1,11 +1,10 @@
 const http = require("http");
 const ws = require("ws");
 
-const apiPort = process.env.API_PORT ?? 3000;
-const wsPort = process.env.WEBSOCKET_PORT ?? 3002;
-
-function run() {
-  const wsServer = new ws.WebSocketServer({ port: wsPort });
+function run(apiPort, wsPort) {
+  const wsServer = new ws.WebSocketServer({ port: wsPort }, () =>
+    console.log(`greedy-http websocket listening on port: ${wsPort}...`)
+  );
 
   http
     .createServer(async (req, res) => {
--- ui.js
+++ ui.js
@@ -1,9 +1,7 @@
 const http = require("http");
 const static = require("node-static");
 
-const uiPort = process.env.UI_PORT ?? 3001;
-
-function run() {
+function run(uiPort) {
   const staticServer = new static.Server(`${__dirname}/ui`);
   http.createServer((req, res) => staticServer.serve(req, res)).listen(uiPort);
 
